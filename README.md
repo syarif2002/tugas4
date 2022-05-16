521ddb68e29d41d7c1676b76a2e2877d3a35c662
syarif2002 <rongodsyarif@gmail.com>
2022-05-16 19:09:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# spring-boot-forgot-password
+
+
+### Image Screen shot
+
+![63b0fec7-6256-4e94-9957-51a3ce5847c6](https://user-images.githubusercontent.com/80630206/164523909-1f479991-9da1-4264-adac-d036a843db22.jpg)
+![d0d2556c-a824-4775-9d91-a42ad4ed36bb](https://user-images.githubusercontent.com/80630206/164523950-643a8469-2a46-43fb-a860-ab0e24a56337.jpg)
+![f065b59e-36d5-433d-8b24-9534ccef6f2a](https://user-images.githubusercontent.com/80630206/164524011-966bc566-e40e-4f53-a35f-ca3fa47685e6.jpg)
+![1e2efa29-4302-4747-9451-41f37814a54b](https://user-images.githubusercontent.com/80630206/164524047-3bbd0673-9ed8-4411-bb08-dc3f4fef6e35.jpg)
+![83de599d-3099-4799-a2ed-84a9b0b177f3](https://user-images.githubusercontent.com/80630206/164524072-260ff30e-148a-441f-822e-ab1312d3abb8.jpg)
+![820a5354-ed2d-4e19-9940-5316610cbb77](https://user-images.githubusercontent.com/80630206/164524104-ce638193-a053-431d-88d7-eca021b3d1ec.jpg)
+![c9baa899-d101-492b-a9e1-3a4db827a82e](https://user-images.githubusercontent.com/80630206/164524139-190970ff-cbd3-4bbc-8784-2b7a3379c525.jpg)
+
---
+++ src/main/java/com/rijki/service/implementation/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,36 @@
+package com.rijki.service.implementation;
+
+import com.rijki.entity.PasswordResetToken;
+import com.rijki.repository.PasswordResetTokenRepository;
+import com.rijki.service.framework.PasswordResetTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/04/22
+ * Time: 08.41
+ */
+@Service
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Autowired
+    public PasswordResetTokenServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+    }
+
+    @Override
+    public PasswordResetToken findByToken(String token) {
+        return passwordResetTokenRepository.findByToken(token).orElse(null);
+    }
+
+    @Override
+    public PasswordResetToken save(PasswordResetToken passwordResetToken) {
+        return passwordResetTokenRepository.save(passwordResetToken);
+    }
+}
